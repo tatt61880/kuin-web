6dec63ebaec60eb3d62144b134c029d8568b4f10
tatt61880 <tatt61880@gmail.com>
2020-07-27 19:04:08 +0900

    Update kuin_web_ui.js

--- js/kuin_web_ui.js
+++ js/kuin_web_ui.js
@@ -30,7 +30,7 @@
 			included = true;
 			addLog("スクリプトをロード中。");
 			var script = document.createElement("script");
-			script.src = "js/kuin.js";
+			script.src = "js/kuin.js?20200727";
 			script.onload = function(){
 				if(!this.readyState || this.readyState === "loaded" || this.readyState === "complete"){
 					addLog("スクリプトがロード完了。");
